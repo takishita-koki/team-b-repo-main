2f16b2b99190ced40bde800511016b69200b5656
takishita-koki <koki.takishita@prum.jp>
2025-03-12 13:08:49 +0900

    kadai4　人気ランキングでいいね数順で表示されるようになりました。

--- app.js
+++ app.js
@@ -139,18 +139,26 @@ app.post('/machines/:id/like', (req, res) => {
   }
 
   // いいね数を増やす処理をここに追加
-
+  db.run(
+    'UPDATE machines SET likes =likes + 1 WHERE id = ?',
+    [machineId],
+    () => {
+    }
+  );
   res.redirect(redirectPath);
 });
 
 // 課題4: 人気順（いいねの多い順）に機械を表示(front・backend)
 app.get('/machines/popular', (req, res) => {
-  // データベースからいいねの多い順に機械の情報を取得する処理をここに追加
-
-  // 取得したデータをpopular_machines.ejsに渡す
-  // ヒント: res.render('ファイル名', {machines: データベースから取得した値})
-  res.redirect('/');
-});
+    // データベースからいいねの多い順に機械の情報を取得する処理をここに追加
+    db.all(
+      'SELECT * FROM machines order by likes desc',[],(error,machines) => {
+         res.render('popular_machines', { machines: machines });
+      }
+     );
+    // 取得したデータをpopular_machines.ejsに渡す
+    // ヒント: res.render('ファイル名', {machines: データベースから取得した値})
+  });
 
 app.post('/rental/:id', (req, res) => {
   const machineId = req.params.id;
